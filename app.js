69fdb4064fe4d6089dab3a0a04466680ae0b6a59
Maxx Crawford <maxx.crawford@gmail.com>
2019-03-02 14:56:08 -0600

    Added mobile styles

---
+++ app.js
@@ -0,0 +1,12 @@
+(function($) {
+"use strict";
+
+	var myArray = ['90s', 'retrowave', 'pizza', 'computer'];
+
+	$(document).on('click', 'h2', function(){
+		var rand = myArray[Math.floor(Math.random() * myArray.length)];
+		var url = "http://tv.giphy.com/" + rand;
+		$('#giphy').attr('src', url);
+	})
+
+})(jQuery);
